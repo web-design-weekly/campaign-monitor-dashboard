810cbd1abdbbbe3a0a3707b919c06c9f50dcbb89
Jake Bresnehan <jakeb@yahoo-inc.com>
2013-06-05 15:50:23 +1000

    commented out script to be inlineq

--- js/admin.js
+++ js/admin.js
@@ -3,21 +3,21 @@
 	$(function () {
 		// Place your administration-specific JavaScript here
 
-       var lineChartData = {
-            labels : ["January","February","March","April","May","June","July"],
-            datasets : [
-                {
-                    fillColor : "rgba(151,187,205,0.5)",
-                    strokeColor : "rgba(151,187,205,1)",
-                    pointColor : "rgba(151,187,205,1)",
-                    pointStrokeColor : "#fff",
-                    data : [28,48,40,19,96,27,100]
-                }
-            ]
+    //    var lineChartData = {
+    //         labels : ["January","February","March","April","May","June","July"],
+    //         datasets : [
+    //             {
+    //                 fillColor : "rgba(151,187,205,0.5)",
+    //                 strokeColor : "rgba(151,187,205,1)",
+    //                 pointColor : "rgba(151,187,205,1)",
+    //                 pointStrokeColor : "#fff",
+    //                 data : [28,48,40,19,96,27,100]
+    //             }
+    //         ]
 
-        }
+    //     }
 
-    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+    // var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
 
 
 	});
