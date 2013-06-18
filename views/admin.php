2cf4fb83a75629e66efb8db54610db277bb4e0ac
Dean Rollins <deano.dinosaur@gmail.com>
2013-06-19 00:35:06 +1000

    Neaten up things into functions, add running total graph
    Signed-off-by: Dean Rollins <deano.dinosaur@gmail.com>

--- views/admin.php
+++ views/admin.php
@@ -47,9 +47,10 @@
     $segments_result = $wrap->get_segments();
 
     if($result->was_successful()) {
+        $total_active_subscribers = $stats_result->response->TotalActiveSubscribers;
         echo "<div class=\"sub-stats\">";
         echo "<p>List - " .$result->response->Title. "</p>";
-        echo "<p>Total Subscribers - " .$stats_result->response->TotalActiveSubscribers. "</p>";
+        echo "<p>Total Subscribers - " .$total_active_subscribers. "</p>";
         echo "</div>";
         //var_dump($result->response);
     } else {
@@ -75,57 +76,75 @@ function get_actives($auth, $page_number)
 
     // params: start date, page number, page size, order by, order direction
     $result = $auth->get_active_subscribers($date_from, $page_number, 1000, 'date', 'asc');
+
+    if($result->was_successful()) {
+        echo "<p>Got subscribers</p>";
+        echo "<pre>";
+
+        echo("ResultsOrderedBy ".$result->response->ResultsOrderedBy."\n");
+        echo("OrderDirection ".$result->response->OrderDirection."\n");
+        echo("PageNumber ".$result->response->PageNumber."\n");
+        echo("PageSize ".$result->response->PageSize."\n");
+        echo("RecordsOnThisPage ".$result->response->RecordsOnThisPage."\n");
+        echo("TotalNumberOfRecords ".$result->response->TotalNumberOfRecords."\n");
+        echo("NumberOfPages ".$result->response->NumberOfPages."\n");
+        echo "</pre>";
+
+    } else {
+        echo "<pre>";
+        echo '<p>Failed with code '.$result->http_status_code."</p>";
+        var_dump($result->response);
+        echo "</pre>";
+    }
+
     return $result;
 }
 
-
-// get the first page of results
-// currently assuming if this succeeds, then all calls for subsequent pages will also succeed
-$result = get_actives($wrap, 1);
-
-echo "Result of GET /api/v3/lists/{ID}/active\n<br />";
-if($result->was_successful()) {
-    echo "Got subscribers\n<br /><pre>";
-
-    echo("ResultsOrderedBy ".$result->response->ResultsOrderedBy."\n");
-    echo("OrderDirection ".$result->response->OrderDirection."\n");
-    echo("PageNumber ".$result->response->PageNumber."\n");
-    echo("PageSize ".$result->response->PageSize."\n");
-    echo("RecordsOnThisPage ".$result->response->RecordsOnThisPage."\n");
-    echo("TotalNumberOfRecords ".$result->response->TotalNumberOfRecords."\n");
-    echo("NumberOfPages ".$result->response->NumberOfPages."\n");
-
-
-    $num_pages = $result->response->NumberOfPages;
-    $stack = array();
-
-    // we've already got page 1; loop from 1 until < num_pages
-    for($i = 1; $i < $num_pages; $i++) {
-        foreach($result->response->Results as $list) {
-
-            $date = $list->Date;
-            // echo $date."\n";
-
-            // // just get the date part of the $list->Date?
-            // $pattern = '([^\s]+)';
-            // preg_match($pattern, $date, $matches);
-            // $d = $matches[0];
-
-            // just get the month part of the $list->Date
-            $d = substr($date, 0, 7);
-
-            array_push($stack, $d);
-        }
-
-        $next_page = $i+1;
-        $result = get_actives($wrap, $next_page);
+function add_results_to_stack($stack, $result)
+{
+    foreach($result->response->Results as $list) {
+        $date = $list->Date;
+        $d = substr($date, 0, 7);   // just get the month part of the $list->Date
+        // could convert this to pretty text for month?
+        array_push($stack, $d);
     }
 
-    // $stack now contains the results for all active subscribers
+    print_r("Size of stack: ".sizeof($stack));
 
-    // print the data
-    print_r(array_count_values($stack));
+    return $stack;
+}
+
+    // **-**
+
+    // start of API calls
+    //
+
+    // get the first page of results
+    $result = get_actives($wrap, 1);
+    $stack = add_results_to_stack(array(), $result);
+
+    // check if we need to get more pages
+    // we've already got page 1; loop from 1 until < num_pages, and get additional pages if necessary
+    $num_pages = $result->response->NumberOfPages;
+    for($i = 1; $i < $num_pages; $i++) {
+        $next_page = $i+1;
+        $result = get_actives($wrap, $next_page);
+        $stack = add_results_to_stack($stack, $result);
+    }
+
+    // $stack now contains the results for all _active_ subscribers
+    // get data for graph
     $graph_data = array_count_values($stack);
+
+    //
+    // end of API calls
+
+    // **-**
+
+
+    // hard-coded test data
+    // to use comment out from **-** to **-**
+    // $graph_data = array("2012-06"=>67, "2012-07"=>185, "2012-08"=>663, "2012-09"=>112, "2012-10"=>160, "2012-11"=>234, "2012-12"=>1569, "2013-01"=>2059, "2013-02"=>824, "2013-03"=>559, "2013-04"=>526, "2013-05"=>1600, "2013-06"=>442);
     
     $php_keys = array_keys($graph_data);
      $js_keys = json_encode($php_keys);
@@ -133,18 +152,39 @@ if($result->was_successful()) {
     $php_vals = array_values($graph_data);
      $js_vals = json_encode($php_vals);
 
-} else {
-    echo 'Failed with code '.$result->http_status_code."\n<br /><pre>";
-    var_dump($result->response);
-}
-echo '</pre>';
+
+    // create data for the running total graph
+
+    $graph_data_running = $graph_data;
+    $len = sizeof($graph_data_running)-1;
+
+    $keys = array_keys($graph_data);
+
+    // set last element of running total as total_active_subscribers
+    // then calculate previous months by working backwords
+    $graph_data_running[$keys[$len]] = $total_active_subscribers;
+    for ($i=$len-1; $i >= 0 ; $i--) { 
+        $graph_data_running[$keys[$i]] = $graph_data_running[$keys[$i+1]] - $graph_data[$keys[$i+1]];
+    }
+    
+    $php_keys_running = array_keys($graph_data_running);
+     $js_keys_running = json_encode($php_keys_running);
+
+    $php_vals_running = array_values($graph_data_running);
+     $js_vals_running = json_encode($php_vals_running);
+
+
+    echo '<pre>';
+        print_r($graph_data);
+        print_r($graph_data_running);
+    echo '</pre>';
 
 ?>
 
 <script type="text/javascript">
 
 jQuery(function () {
-  var lineChartData = {
+    var lineChartDataMonthly = {
             labels : <?php echo $js_keys; ?>,
             datasets : [
                 {
@@ -155,12 +195,27 @@ jQuery(function () {
                     data : <?php echo $js_vals; ?>
                 }
             ]
-
         }
 
-    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+    var lineChartDataRunning = {
+            labels : <?php echo $js_keys_running; ?>,
+            datasets : [
+                {
+                    fillColor : "rgba(151,187,205,0.5)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    pointColor : "rgba(151,187,205,1)",
+                    pointStrokeColor : "#fff",
+                    data : <?php echo $js_vals_running; ?>
+                }
+            ]
+        }
+    var optionsRunning = {bezierCurve : false};
+
+    var myLine = new Chart(document.getElementById("canvas-graph-1").getContext("2d")).Line(lineChartDataMonthly);
+    var myLine = new Chart(document.getElementById("canvas-graph-2").getContext("2d")).Line(lineChartDataRunning, optionsRunning);
 });
 
 </script>
 
- <canvas id="canvas" width="700" height="400"></canvas>
+ <canvas id="canvas-graph-1" width="700" height="400"></canvas>
+ <canvas id="canvas-graph-2" width="700" height="400"></canvas>
